cf2a473acd25d4c3126721071f837ad65f69c0a9
aileron <aileron.cc@gmail.com>
2012-02-13 01:28:19 +0900

    キチンと環境を構築する為に、git管理を始めてみる

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+for file in ".zshrc" ".vimrc" ".vim"
+do
+    src="$HOME/dotfiles/$file"
+    dist="$HOME/$file"
+    printf "ln -s $src $dist"
+    if [ -e $src ] && [ ! -e $dist ]
+    then
+        ln -s $src $dist
+        printf " create ..\n"
+    else
+        printf " skip ..\n"
+    fi
+done
